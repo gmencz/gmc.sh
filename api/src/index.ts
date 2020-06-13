f11c71ae04f6130c20aae424e26bf75e13bea0c5
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-06-13 04:22:55 +0200

    :text: add comments with expected functionality for redirections

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,15 +3,24 @@ import { greenBright } from 'chalk';
 
 const api = express();
 
+api.get('/', (_, res) => {
+  const redirectDestination =
+    process.env.NODE_ENV === 'production'
+      ? 'https://app.gmc.sh'
+      : 'http://localhost:3000';
+
+  res.redirect(redirectDestination);
+});
+
 api.get('/:slug', (req, res) => {
   const { slug } = req.params;
-
-  if (!slug) {
-    res.status(400).json({
-      message: 'No slug was specified',
-    });
-  }
-
+  /*
+    Find ShortenedResouce in Dgraph with the slug identifier
+    If it exists, redirect to the externalFQDN from that 
+    ShortenedResource, if it doesn't, redirect to a route
+    on "https://app.gmc.sh" where it would display a
+    not found page.
+  */
   res.json({ slug });
 });
 
