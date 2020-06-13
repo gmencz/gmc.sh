0f1de170788904f88b472492ef3f24c10be0a710
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-06-13 16:24:06 +0200

    :sparkles: add 404 route controller

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,8 +7,8 @@ import {
 
 const api = express();
 
-api.get('/', redirectToApp);
 api.get('/:slug', redirectBySlug);
+api.get('*', redirectToApp);
 
 const PORT = process.env.PORT || 8081;
 api.listen(PORT, () => {
