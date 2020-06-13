9cca7d09e858eed1cf03cbe0deb17207b69b61cd
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-06-13 05:44:52 +0200

    :sparkles: add business logic for shortened urls redirections

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
 import { greenBright } from 'chalk';
+import { DgraphClientStub, DgraphClient } from 'dgraph-js-http';
 
 const api = express();
+
+interface FindShortenedResourceData {
+  shortenedResourceResult: { 'ShortenedResource.originalFQDN': string }[];
+}
+
+const dgraphClientStub = new DgraphClientStub('http://localhost:8080');
+const dgraphClient = new DgraphClient(dgraphClientStub);
 
 api.get('/', (_, res) => {
   const redirectDestination =
@@ -9,19 +17,61 @@ api.get('/', (_, res) => {
       ? 'https://app.gmc.sh'
       : 'http://localhost:3000';
 
-  res.redirect(redirectDestination);
+  return res.redirect(redirectDestination);
 });
 
-api.get('/:slug', (req, res) => {
+api.get('/:slug', async (req, res) => {
   const { slug } = req.params;
-  /*
-    Find ShortenedResouce in Dgraph with the slug identifier
-    If it exists, redirect to the externalFQDN from that 
-    ShortenedResource, if it doesn't, redirect to a route
-    on "https://app.gmc.sh" where it would display a
-    not found page.
+
+  // Dgraph query to find resource by slug
+  const shortenedResourceQuery = `
+    query ShortenedResourceBySlug($slug: string) {
+      shortenedResourceResult(func: eq(ShortenedResource.slug, $slug)) {
+        ShortenedResource.originalFQDN
+      }
+    }
+  `;
+
+  try {
+    // Start dgraph query transaction with slug from query params
+    const shortenedResourceQueryVars = { $slug: slug };
+    const queryRes = await dgraphClient
+      .newTxn()
+      .queryWithVars(shortenedResourceQuery, shortenedResourceQueryVars);
+
+    const data = queryRes.data as FindShortenedResourceData;
+
+    /*
+    If there's no shortened URL with the specified slug,
+    redirect to a not found route so the end user
+    knows what happened
   */
-  res.json({ slug });
+    if (data.shortenedResourceResult.length !== 1) {
+      const redirectDestination =
+        process.env.NODE_ENV === 'production'
+          ? 'https://app.gmc.sh/error?code=not_found'
+          : 'http://localhost:3000/error?code=not_found';
+
+      return res.redirect(redirectDestination);
+    }
+
+    /*
+    Get the original FQDN (Fully qualified domain name) and redirect
+    to it
+  */
+
+    const shortenedResourceOriginalFQDN =
+      data.shortenedResourceResult[0]['ShortenedResource.originalFQDN'];
+
+    return res.redirect(shortenedResourceOriginalFQDN);
+  } catch (error) {
+    const redirectDestination =
+      process.env.NODE_ENV === 'production'
+        ? 'https://app.gmc.sh/error?code=unknown'
+        : 'http://localhost:3000/error?code=unknown';
+
+    return res.redirect(redirectDestination);
+  }
 });
 
 const PORT = process.env.PORT || 8081;
