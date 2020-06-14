9bc82fad5823649d0730ac12c693f6d258b2e708
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-06-14 19:29:51 +0200

    :sparkles: add functionality to create/generate slugs for the shortened URL and validate data client side

--- web/src/ts/main.ts
+++ web/src/ts/main.ts
@@ -1,4 +1,6 @@
 import { request } from 'graphql-request';
+import { nanoid } from 'nanoid';
+import * as clipboard from 'clipboard-polyfill';
 
 interface NewShortenedUrlMutationVariables {
   urlToShorten: string;
@@ -21,6 +23,18 @@ const urlShortenerForm = document.querySelector(
   '.url-shortener'
 ) as HTMLFormElement;
 
+const urlInput = document.querySelector(
+  'input#url-to-shorten'
+) as HTMLInputElement;
+
+const errorBox = document.querySelector('.error-box') as HTMLDivElement;
+const successBox = document.querySelector('.shortened-box') as HTMLDivElement;
+
+urlInput.addEventListener('keydown', () => {
+  errorBox.classList.add('invisible');
+  successBox.classList.add('invisible');
+});
+
 urlShortenerForm.addEventListener('submit', async (event) => {
   if (event.preventDefault) {
     event.preventDefault();
@@ -37,6 +51,11 @@ urlShortenerForm.addEventListener('submit', async (event) => {
   ) as HTMLInputElement).value;
 
   // TODO: Validate form stuff
+
+  if (!urlInput.checkValidity()) {
+    errorBox.classList.remove('invisible');
+    return;
+  }
 
   const newShortenedURLMutation = `
     mutation ShortenURL(
@@ -56,25 +75,39 @@ urlShortenerForm.addEventListener('submit', async (event) => {
 
   const newShortenedURLMutationVariables: NewShortenedUrlMutationVariables = {
     urlToShorten,
-    slugForShortenedURL,
+    slugForShortenedURL: slugForShortenedURL ? slugForShortenedURL : nanoid(4),
   };
 
+  const submitButton = document.querySelector(
+    'form.url-shortener button[type="submit"]'
+  ) as HTMLButtonElement;
+
   try {
+    submitButton.disabled = true;
     const response: NewShortenedUrlMutationResponse = await request(
       'http://localhost:8080/graphql',
       newShortenedURLMutation,
       newShortenedURLMutationVariables
     );
 
-    // Here's the shortened url!
-    console.log(
+    submitButton.disabled = false;
+    await clipboard.writeText(
       `https://gmc.sh/${response.addShortenedResource.shortenedresource[0].slug}`
     );
+    (successBox.querySelector(
+      'strong'
+    ) as HTMLElement).innerHTML = `The shortened URL <strong>https://gmc.sh/${response.addShortenedResource.shortenedresource[0].slug}</strong> was copied to your clipboard.`;
+    successBox.classList.remove('invisible');
+    return;
   } catch (error) {
-    console.log(error);
+    submitButton.disabled = false;
     const splitByBracket = error.message.split('{');
+
     if (splitByBracket.length === 0) {
-      console.error(`Something went wrong, we're working on a fix`);
+      (errorBox.querySelector(
+        'strong'
+      ) as HTMLElement).textContent = `Something went wrong, we're working on a fix`;
+      return;
     }
 
     const [, ...rawGraphQLInfo] = splitByBracket;
@@ -86,7 +119,25 @@ urlShortenerForm.addEventListener('submit', async (event) => {
     );
 
     if (slugAlreadyExistsError) {
-      console.error('Slug already exists');
+      const generatedSlug = nanoid(4);
+      const variables: NewShortenedUrlMutationVariables = {
+        slugForShortenedURL: generatedSlug,
+        urlToShorten,
+      };
+
+      const response: NewShortenedUrlMutationResponse = await request(
+        'http://localhost:8080/graphql',
+        newShortenedURLMutation,
+        variables
+      );
+
+      await clipboard.writeText(
+        `https://gmc.sh/${response.addShortenedResource.shortenedresource[0].slug}`
+      );
+      (successBox.querySelector(
+        'strong'
+      ) as HTMLElement).innerHTML = `The shortened URL <strong>https://gmc.sh/${response.addShortenedResource.shortenedresource[0].slug}</strong> was generated and copied to your clipboard because the slug you provided is already in use.`;
+      successBox.classList.remove('invisible');
     }
   }
 });
