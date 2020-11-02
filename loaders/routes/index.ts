bc474303ad85dd6498656b7a42ad9063718fc005
Gabriel Méndez <yo@gabrielmendezc.com>
2020-11-02 22:04:06 +0100

    :sparkles: Set up prisma
    Signed-off-by: Gabriel Méndez <yo@gabrielmendezc.com>

--- loaders/routes/index.ts
+++ loaders/routes/index.ts
@@ -1,20 +1,8 @@
 import type { DataLoader } from "@remix-run/core";
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-const todos: Todo[] = [
-  { id: 1, title: "Learn Remix", completed: false },
-  { id: 2, title: "Build amazing app", completed: false },
-  { id: 3, title: "Tweet about Remix", completed: false },
-];
-
 let loader: DataLoader = async () => {
   return {
-    todos,
+    message: "hello",
   };
 };
 
