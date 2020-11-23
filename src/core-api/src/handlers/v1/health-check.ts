7229c92fa241c6756fd13657e9bd62398b95a298
Gabriel <yo@gabrielmendezc.com>
2020-11-23 04:15:10 +0100

    :sparkles: Move from ts-node-dev to nodemon and ts-node
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/core-api/src/handlers/v1/health-check.ts
+++ src/core-api/src/handlers/v1/health-check.ts
@@ -1,12 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
-import firebase from 'firebase'
 
 async function healthCheck(
   _: FastifyRequest,
   reply: FastifyReply,
 ): Promise<FastifyReply> {
-  console.log(firebase.auth().currentUser)
-
   return reply.send({
     status: 'OK',
   })
