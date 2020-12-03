ce541c40369b840671f81c88372be24a876311c8
Gabriel <yo@gabrielmendezc.com>
2020-12-03 01:37:30 +0100

    :pencil: Document 0.0.0.0 change in fastify server
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/core-api/src/index.ts
+++ src/core-api/src/index.ts
@@ -2,6 +2,9 @@ import { build } from './server'
 
 const server = build()
 
+// We need the 0.0.0.0 so fastify listens on all interfaces
+// if we don't set this then Traefik or any other reverse proxy
+// would not be able to access this.
 server.listen(Number(process.env.PORT), '0.0.0.0', (err, address) => {
   if (err) {
     server.log.error(err.message)
