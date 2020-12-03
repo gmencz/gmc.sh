a64912c1f6fdf05267c0d23362955f4ab6d08cc0
Gabriel <yo@gabrielmendezc.com>
2020-12-04 00:56:14 +0100

    :white_check:mark: Add tests for sign in form and setup env with msw and fetch polyfill
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/web/jest.config.js
+++ src/web/jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  setupFiles: [require.resolve('whatwg-fetch')],
+  setupFilesAfterEnv: ['<rootDir>/test/setup-env.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
 }
