51937902e954a83cb501f92cf855aafc5a02c59a
Gabriel <yo@gabrielmendezc.com>
2020-12-03 02:45:05 +0100

    :sparkles: Update API constant for prod environment
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/web/utils/constants.ts
+++ src/web/utils/constants.ts
@@ -1,3 +1,6 @@
-const API_ENDPOINT = 'http://localhost:4500'
+const API_ENDPOINT =
+  process.env.NODE_ENV === 'production'
+    ? 'https://api.gmc.sh'
+    : 'http://localhost:4500'
 
 export { API_ENDPOINT }
