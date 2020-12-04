c04bb7fb166547f6dd006835caecc3f4d1ab703f
Gabriel <yo@gabrielmendezc.com>
2020-12-04 01:42:58 +0100

    :white_check:mark: Add tests for mobile menu
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/web/components/sign-in-form.tsx
+++ src/web/components/sign-in-form.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { Fragment } from 'react'
 import { useForm } from 'react-hook-form'
 import { QueryStatus, useMutation } from 'react-query'
 import * as yup from 'yup'
@@ -114,28 +115,30 @@ function SignInForm() {
           </span>
           Sign in
           {status === QueryStatus.Loading && (
-            <svg
-              className="animate-spin ml-3 h-5 w-5 text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              role="alert"
-            >
+            <Fragment>
               <span className="sr-only">loading...</span>
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
+              <svg
+                className="animate-spin ml-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                role="alert"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+            </Fragment>
           )}
         </button>
       </div>
