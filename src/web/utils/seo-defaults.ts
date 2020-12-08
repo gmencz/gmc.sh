40afe6ac491783a588fb5cd8e6e608f0998907bb
Gabriel <yo@gabrielmendezc.com>
2020-12-08 02:08:42 +0100

    :sparkles: Update Seoo defaults
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/web/utils/seo-defaults.ts
+++ src/web/utils/seo-defaults.ts
@@ -2,7 +2,7 @@ const seoDefaults = {
   title: 'Gmc.sh • Free, short custom URLs',
   description:
     'Free, short and custom URLs, get rid of those long and boring links.',
-  image: 'https://app.gmc.sh/hero.svg',
+  image: 'https://app.gmc.sh/hero.png',
   keywords: 'URL Shortener, Short links, Free URL Shortener, URL, Link',
   pagePath: '/',
 }
