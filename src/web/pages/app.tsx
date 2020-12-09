9c8d1ce33cde66443b08601b4b1500b4169b37bc
Gabriel <yo@gabrielmendezc.com>
2020-12-09 17:50:50 +0100

    :sparkles: Create under development page for the /app
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/web/pages/app.tsx
+++ src/web/pages/app.tsx
@@ -1,4 +1,7 @@
+import Footer from 'components/footer'
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import { Fragment } from 'react'
 import { seoDefaults } from 'utils/seo-defaults'
 import {
@@ -32,7 +35,34 @@ function App({ user }: AppProps) {
         <meta name="twitter:description" content={seoDefaults.description} />
         <meta name="twitter:image" content={seoDefaults.image} />
       </Head>
-      <p>Hello {user.username} 👋</p>
+      <div className="bg-gray-50 min-h-screen flex">
+        <div className="p-4 flex flex-col items-center text-center max-w-lg mx-auto justify-center">
+          <h3 className="text-2xl font-extrabold text-gray-900 tracking-tight sm:text-3xl">
+            Hi {user.username}.
+          </h3>
+          <p className="mt-3 text-lg text-gray-500">
+            Thank you for becoming one of our early users, we&apos;ll keep it in
+            mind. The app is currently under development so make sure to
+            subscribe to our newsletter{' '}
+            <Link href="/">
+              <a className="font-medium text-indigo-600 hover:text-indigo-500">
+                here
+              </a>
+            </Link>{' '}
+            to get notified when it&apos;s ready!
+          </p>
+          <div className="mt-4">
+            <Image
+              src="/under_development.svg"
+              alt="Under development"
+              width="600px"
+              height="600px"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </Fragment>
   )
 }
