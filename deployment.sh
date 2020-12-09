e29550678985857c6693396ecffd8c2cf8a57bcc
Gabriel <yo@gabrielmendezc.com>
2020-12-09 20:51:05 +0100

    :construction_worker: Update deploy.yml
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- deployment.sh
+++ deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-if [ $(docker ps -f name=blue-gmc-sh -q) ]
+if [ "$(docker ps -f name=blue-gmc-sh -q)" ]
 then
     BG_NEW="green-gmc-sh"
     BG_OLD="blue-gmc-sh"
@@ -12,11 +12,11 @@ echo "Pulling latest images from Github Package Registry"
 sudo docker-compose -f docker-compose.prod-apps.yml --project-name=$BG_NEW pull
 sudo docker-compose -f docker-compose.prod-apps.yml --project-name=$BG_NEW build --no-cache
 sleep 2s
-echo "Starting "$BG_NEW" containers"
+echo "Starting $BG_NEW containers"
 sudo docker-compose -f docker-compose.prod-apps.yml --project-name=$BG_NEW up -d --force-recreate
 
 echo "Waiting..."
 sleep 20s
 
-echo "Stopping "$BG_OLD" containers"
+echo "Stopping $BG_OLD containers"
 sudo docker-compose -f docker-compose.prod-apps.yml --project-name=$BG_OLD stop
