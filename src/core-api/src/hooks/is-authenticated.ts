1c3c3f7bb2e3be1503ad6025050f963949601fc3
Gabriel <yo@gabrielmendezc.com>
2020-12-09 20:33:38 +0100

    :pencil: Add logging to isauthenticated hook
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/core-api/src/hooks/is-authenticated.ts
+++ src/core-api/src/hooks/is-authenticated.ts
@@ -5,6 +5,7 @@ const isAuthenticatedHook: preValidationHookHandler = async (
   request,
   reply,
 ) => {
+  reply.log.info(request.cookies)
   const sessionData = request.session.get('data')
   if (!sessionData) {
     reply.status(401).send({
