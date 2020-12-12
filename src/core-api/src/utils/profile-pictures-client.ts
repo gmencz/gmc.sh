d5aa849bace24215a2f82945089d82d090d9658e
Gabriel <yo@gabrielmendezc.com>
2020-12-12 07:53:57 +0100

    :white_check_mark: Maybe fix tests?
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- src/core-api/src/utils/profile-pictures-client.ts
+++ src/core-api/src/utils/profile-pictures-client.ts
@@ -1,7 +1,7 @@
 import { BlobServiceClient } from '@azure/storage-blob'
 
-const AZURE_STORAGE_CONNECTION_STRING = process.env
-  .AZURE_STORAGE_CONNECTION_STRING as string
+const AZURE_STORAGE_CONNECTION_STRING =
+  process.env.AZURE_STORAGE_CONNECTION_STRING || '<azureConnection>'
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(
   AZURE_STORAGE_CONNECTION_STRING,
