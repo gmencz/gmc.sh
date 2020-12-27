0d24a0698859636cd67f67cdef0b10ff72d7e88e
Gabriel <yo@gabrielmendezc.com>
2020-12-27 03:32:14 +0100

    :white_check_mark: Update test to pass
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- utils/__tests__/with-auth-server-side-props.ts
+++ utils/__tests__/with-auth-server-side-props.ts
@@ -24,6 +24,33 @@ test('appends props returned by custom getServerSideProps function to final getS
 
   expect(result.props).toMatchInlineSnapshot(`
     Object {
+      "dehydratedState": Object {
+        "mutations": Array [],
+        "queries": Array [
+          Object {
+            "queryHash": "\\"/api/me\\"",
+            "queryKey": "/api/me",
+            "state": Object {
+              "data": Object {
+                "email": "test@example.com",
+                "id": "1",
+                "username": "test",
+              },
+              "dataUpdateCount": 1,
+              "dataUpdatedAt": 1609036318452,
+              "error": null,
+              "errorUpdateCount": 0,
+              "errorUpdatedAt": 0,
+              "fetchFailureCount": 0,
+              "fetchMeta": null,
+              "isFetching": false,
+              "isInvalidated": false,
+              "isPaused": false,
+              "status": "success",
+            },
+          },
+        ],
+      },
       "id": 10,
       "user": Object {
         "email": "test@example.com",
