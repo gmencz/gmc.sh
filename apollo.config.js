b83c13f05cf177ac4f2276dc023bb780558a3c80
Gabriel <yo@gabrielmendezc.com>
2021-01-08 17:53:33 +0100

    :art: Cleanup
    Signed-off-by: Gabriel <yo@gabrielmendezc.com>

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config({ path: '.env.local' })
 
-console.log(process.env.HASURA_ADMIN_SECRET)
-
 module.exports = {
   client: {
     includes: ['./gql/**/*.graphql'],
